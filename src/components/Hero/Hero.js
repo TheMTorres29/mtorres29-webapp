48863e8646062623dfa25b8ea9b8b566d82497a9
TheMTorres29 <TheMTorres29@yahoo.com>
2023-04-13 16:36:47 -0700

    Hero video

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../../App';
 import './Hero.css';
-// import BanjoHero from '../../assets/videos/InkyBanjoHero540.mp4';
+import BanjoHero from '../../assets/videos/InkyBanjoHero.mp4';
 
 function Hero() {
   return (
     <div className='hero-container'>
-        {/* <video src={ BanjoHero } autoPlay loop muted /> */}
+        <video src={ BanjoHero } autoPlay loop muted />
         <h1>Miguel Torres</h1>
         <p>Another SoCal Banjo main GUH HUH</p>
     </div>
