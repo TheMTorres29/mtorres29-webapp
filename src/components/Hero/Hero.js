fdba92aa1a99d9e5e82a050056f58669220679c7
TheMTorres29 <TheMTorres29@yahoo.com>
2023-04-14 14:56:52 -0700

    Adjusted Hero video CSS

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../../App';
 import './Hero.css';
-import BanjoHero from '../../assets/videos/InkyBanjoHero.mp4';
+// import BanjoHero from '../../assets/videos/InkyBanjoHero.mp4';
 
 function Hero() {
   return (
     <div className='hero-container'>
-        <video src={ BanjoHero } autoPlay loop muted />
-        <h1>Miguel Torres</h1>
-        <p>Another SoCal Banjo main GUH HUH</p>
+        {/* <video src={ BanjoHero } autoPlay loop /> */}
+        <div className='video-container'>
+          <iframe src="https://www.youtube.com/embed/o0oZmlrdOz8?controls=0&autoplay=1&mute=1&playsinline=1&playlist=o0oZmlrdOz8&loop=1" title='yt-iframe'></iframe>
+        </div>
+        <div className='hero-text'>
+          <h1>Miguel Torres</h1>
+          <p>Another SoCal Banjo main GUH HUH</p>
+        </div>
+        
     </div>
   )
 }
