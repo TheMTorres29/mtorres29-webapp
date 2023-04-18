c9d4acc2776e1e9d996e6d92a245a1c08c9f9a95
TheMTorres29 <TheMTorres29@yahoo.com>
2023-04-18 07:03:10 -0700

    Made bg a gif for mobile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,11 +8,11 @@ function Hero() {
     <div className='hero-container'>
         {/* <video src={ BanjoHero } autoPlay loop /> */}
         <div className='video-container'>
-          <iframe src="https://www.youtube.com/embed/o0oZmlrdOz8?controls=0&autoplay=1&mute=1&playsinline=1&playlist=o0oZmlrdOz8&loop=1" title='yt-iframe'></iframe>
+          <iframe src="https://www.youtube.com/embed/o0oZmlrdOz8?controls=0&autoplay=1&mute=1&playsinline=1&playlist=o0oZmlrdOz8&loop=1" title='yt-iframe' className='hero-iframe'></iframe>
         </div>
         <div className='hero-text'>
           <h1>Miguel Torres</h1>
-          <p>Another SoCal Banjo main GUH HUH</p>
+          <p>SoCal Web Developer | Banjo Main</p>
         </div>
         
     </div>
