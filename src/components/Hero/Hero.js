4871e207e203785b9419a49724db1fa436f6a0d6
TheMTorres29 <TheMTorres29@yahoo.com>
2023-04-19 17:00:47 -0700

    HeroVideo now from Vimeo for 720p

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,7 +8,8 @@ function Hero() {
     <div className='hero-container'>
         {/* <video src={ BanjoHero } autoPlay loop /> */}
         <div className='video-container'>
-          <iframe src="https://www.youtube.com/embed/o0oZmlrdOz8?controls=0&autoplay=1&mute=1&playsinline=1&playlist=o0oZmlrdOz8&loop=1&hd=1" title='yt-iframe' className='hero-iframe'></iframe>
+          <iframe title="vimeo-player" src="https://player.vimeo.com/video/819312432?h=df2ca32b36&background=1&autoplay=1&quality=720p" frameborder="0" className='hero-iframe'></iframe>
+          {/* <iframe src="https://www.youtube.com/embed/o0oZmlrdOz8?controls=0&autoplay=1&mute=1&playsinline=1&playlist=o0oZmlrdOz8&loop=1" title='yt-iframe' className='hero-iframe'></iframe> */}
         </div>
         <div className='hero-text'>
           <h1>Miguel Torres</h1>
