906d25d8ded06b5fab6c25f9df466e2a0f67205c
TheMTorres29 <TheMTorres29@yahoo.com>
2024-03-25 14:47:34 -0700

    New Custom Footer Icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer({setIsModalOpen}) {
                     <li className="footer-item">
                         <button className='footer-contact-btn' onClick={() => {setIsModalOpen(true);}}>
                             <h4>Contact Me</h4>
-                            <img src='https://img.icons8.com/ios-filled/50/message-squared.png' alt="logo-icon" className="footer-icon" />
+                            <img src='https://drive.google.com/thumbnail?id=1BFi_ZOt5vlvb2O5F4wnYU5ThOZQ3FWf_&export=download' alt="logo-icon" className="footer-icon" />
                         </button>
                     </li>
                 </ul>
@@ -28,13 +28,13 @@ function Footer({setIsModalOpen}) {
                     <li className="footer-item">
                         <a href="https://www.linkedin.com/in/mtorres29/">
                             <h4>Linkedin</h4>
-                            <img src='https://img.icons8.com/ios-filled/50/linkedin.png' alt="logo-icon" className="footer-icon" />
+                            <img src='https://drive.google.com/thumbnail?id=1fRRQ5DN-7kdTpbPy1dbTvkJzIK9XgVkm&export=download' alt="logo-icon" className="footer-icon" />
                         </a>
                     </li>
                     <li className='footer-item'>
                         <a href="https://github.com/TheMTorres29">
                             <h4>GitHub</h4>
-                            <img src='https://icons8.com/icon/62856/github' alt="logo-icon" className="footer-icon" />
+                            <img src='https://drive.google.com/thumbnail?id=1uHWqd4VNLDQSP4mvZAlGZJM1W8_einzE&export=download' alt="logo-icon" className="footer-icon" />
                         </a>
                     </li>
                 </ul>
@@ -47,7 +47,7 @@ function Footer({setIsModalOpen}) {
                     <li className="footer-item">
                         <a href="https://www.twitch.tv/mtorres29">
                             <h4>Twitch</h4>
-                            <img src='https://img.icons8.com/ios-filled/50/twitch.png' alt="logo-icon" className="footer-icon" />
+                            <img src='https://drive.google.com/thumbnail?id=1NYKhuSSqNv2OwlM_wWaPVqRvSXVNk9al&export=download' alt="logo-icon" className="footer-icon" />
                         </a>
                         
                     </li>
