349e4e625585ba1a73686e6689ca02484ac3938c
TheMTorres29 <TheMTorres29@yahoo.com>
2024-03-25 12:50:06 -0700

    Footer icon edits

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer({setIsModalOpen}) {
                     <li className="footer-item">
                         <button className='footer-contact-btn' onClick={() => {setIsModalOpen(true);}}>
                             <h4>Contact Me</h4>
-                            <img src='https://drive.google.com/thumbnail?id=1yyLC1WxwP_YKRzS60ByFwIutiYxACAcV&export=download' alt="logo-icon" className="footer-icon" />
+                            <img src='https://img.icons8.com/ios-filled/50/message-squared.png' alt="logo-icon" className="footer-icon" />
                         </button>
                     </li>
                 </ul>
@@ -28,13 +28,13 @@ function Footer({setIsModalOpen}) {
                     <li className="footer-item">
                         <a href="https://www.linkedin.com/in/mtorres29/">
                             <h4>Linkedin</h4>
-                            <img src='https://drive.google.com/thumbnail?id=1yXzFS32Aw1iY8u5Bk7Iea9vln1Ao2wzs&export=download' alt="logo-icon" className="footer-icon" />
+                            <img src='https://img.icons8.com/ios-filled/50/linkedin.png' alt="logo-icon" className="footer-icon" />
                         </a>
                     </li>
                     <li className='footer-item'>
                         <a href="https://github.com/TheMTorres29">
                             <h4>GitHub</h4>
-                            <img src='https://drive.google.com/thumbnail?id=1leZ2DxuUxx7-PVxjIsZUxM0pQc7ob9is&export=download' alt="logo-icon" className="footer-icon" />
+                            <img src='https://drive.google.com/thumbnail?id=10-06hP8plHj-YmamUzI2RMm2luI7ga4j&export=download' alt="logo-icon" className="footer-icon" />
                         </a>
                     </li>
                 </ul>
@@ -47,14 +47,14 @@ function Footer({setIsModalOpen}) {
                     <li className="footer-item">
                         <a href="https://twitter.com/TheMTorres29">
                             <h4>X</h4>
-                            <img src='https://drive.google.com/thumbnail?id=15nP_vsWCqcvGdenLCdO5JfiNuZMQOxcW&export=download' alt="logo-icon" className="footer-icon" />
+                            <img src='https://img.icons8.com/ios-filled/50/twitterx--v1.png' alt="logo-icon" className="footer-icon" />
                         </a>
                         
                     </li>
                     <li className="footer-item">
                         <a href="https://www.twitch.tv/mtorres29">
                             <h4>Twitch</h4>
-                            <img src='https://drive.google.com/thumbnail?id=1xPiw70TSklXtA7DmRysDoMHt_YVB4ZPE&export=download' alt="logo-icon" className="footer-icon" />
+                            <img src='https://drive.google.com/thumbnail?id=1RyNcq9424hTS1yhHvCwCFUELNmGQjDyc&export=download' alt="logo-icon" className="footer-icon" />
                         </a>
                         
                     </li>
