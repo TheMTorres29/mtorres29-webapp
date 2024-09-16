283466f945498d2e3ee5c2beaad50eebd58f3eac
TheMTorres29 <TheMTorres29@yahoo.com>
2024-09-16 11:45:23 -0700

    Timeline and Icon updates

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -16,6 +16,7 @@ function Timeline() {
         {id:6, ttitle:'GameStop', tdate:'Oct 2020 - May 2021', ticon:'https://drive.google.com/thumbnail?id=1BFft1N-g4A96n6CvwX6h57IqT_XT4AJg', tdesc:'Senior Game Advisor'},
         {id:7, ttitle:'Zumiez DTG', tdate:'Oct 2022 - Oct 2023', ticon:'https://drive.google.com/thumbnail?id=1BdqPJC4yxn6LDJiX5dfdD-GnnXhfx1Xh', tdesc:'Operator / Quality Control'},
         {id:8, ttitle:"Sam's Club", tdate:'Oct 2023 - May 2024', ticon:'https://drive.google.com/thumbnail?id=1aJeaJyTCDHCSO-PNB03nOS7VbmYKStRk', tdesc:'Fresh Dept. Associate'},
+        {id:9, ttitle:"ProLogistix", tdate:'May 2024 - Present', ticon:'https://drive.google.com/thumbnail?id=1hA_U6w6V04KIn96b7I1zI6aovR0spWXu', tdesc:'Recieving Lead'},
     ]
 
     useEffect(() => {
