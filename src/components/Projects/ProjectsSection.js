42f595afc4c4a96d6f7a27c367ad7c18d2afe5ad
TheMTorres29 <TheMTorres29@yahoo.com>
2025-09-02 08:08:01 -0700

    project updates

--- src/components/Projects/ProjectsSection.js
+++ src/components/Projects/ProjectsSection.js
@@ -9,15 +9,33 @@ import fightstickPage from '../../pages/Fightstick.js';
 function ProjectsSection() {
   const projectGallery = [
     {
+      icon: 'https://drive.google.com/thumbnail?id=1SLWNGXydBfeH-7wFRzgA2vlaGlK6mQgf&export=download',
+      link: 'https://github.com/TheMTorres29/RandomSmashdown-Tkinter',
+      title: '[TKinter] Random Smashdown',
+      desc: 'A random character generator for Super Smash Bros. Ultimate using Python and TKinter.',
+    },
+    {
+      icon: 'https://drive.google.com/thumbnail?id=1rFG_kw6Ote7cvwEv63mE5WR9X6lFl-Nd&export=download',
+      link: 'https://ec2-3-95-67-134.compute-1.amazonaws.com/',
+      title: '[Flask] Random Smashdown',
+      desc: 'A random character generator for Super Smash Bros. Ultimate using Python and Flask.',
+    },
+    {
+      icon: 'https://drive.google.com/thumbnail?id=1PpQHmXYbqDf-iR5eoExGoyGJ_tRZcLsp&export=download',
+      link: 'https://themtorres29.github.io/MTCG-PokeStop-Page/',
+      title: '[ReactJS] MTCG PokeStop Page',
+      desc: 'A simple PokeStop landing page for the MTCG Card shop using HTML and CSS.',
+    },
+    {
       icon: 'https://drive.google.com/thumbnail?id=1wU-jPVm9WqBKH4HIWlF9uiSqDPAqjeOR&export=download',
       link: 'https://random-smashdown.site',
-      title: 'Random-Smashdown',
-      desc: 'Fighter Randomizer for Super Smash Bros Ultimate',
+      title: '[ReactJS] Random-Smashdown',
+      desc: 'The original ReactJS Fighter Randomizer for Super Smash Bros Ultimate',
     },
     {
       icon: 'https://drive.google.com/thumbnail?id=1i6Pb2SYeFOuiehyGQXEx6IpEs3919xzv&export=download',
       link: 'https://github.com/TheMTorres29/TropaMagicaGameBuild/releases',
-      title: 'Tropa Magica Game v.5',
+      title: '[Unity] Tropa Magica Game v.5',
       desc: 'First Unity game I built for Game Design course.',
     },
     {
